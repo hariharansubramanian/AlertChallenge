ba5b5614fb3cea00d3bf368b7cedf01dba56a151
Hariharan Subramanian <hariharan1990.s@gmail.com>
2017-07-24 03:00:53 +0530

    * Wrote test cases covering all scenarios regarding POST /alerts     *Asserted expected behaviors while Posting corrupted,missing or duplicate alert data     *Asserted expected success on posting a valid alert
    * Created json files for each test case scenario with same name as the test method

--- src/integrationTest/java/life.dev.hari.alertChallenge/controller/AlertControllerIntegrationTest.java
+++ src/integrationTest/java/life.dev.hari.alertChallenge/controller/AlertControllerIntegrationTest.java
@@ -4,44 +4,104 @@ import life.dev.hari.alertChallenge.WebIntegrationTestBase;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+
+import java.nio.file.Files;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by plank-hari.s on 7/23/2017.
  */
 public class AlertControllerIntegrationTest extends WebIntegrationTestBase {
 
+    public static final String ALERTS_URL_PATH = "/alerts";
+
     @Before
     public void setup() throws Exception {
         super.setup();
     }
 
-
+    /**
+     * POST /alerts test cases
+     */
     @Test
     public void postAlertMustReturnBadRequestForNullReferenceId() throws Exception {
-        Assert.fail();
+        //TODO: Extract the 2 lines below into a method called "getJsonData(jsonResourceFile)"
+        ClassPathResource postJsonResourceFile = new ClassPathResource("testData/postAlert/postAlertMustReturnBadRequestForNullReferenceId.json");
+        byte[] alertJsonWithNullReferenceId = Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+
+        this.mockMvc.perform(post(ALERTS_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(alertJsonWithNullReferenceId))
+                .andExpect(status().isBadRequest())
+                .andReturn();
     }
 
 
     @Test
     public void postAlertMustReturnBadRequestForNullDelay() throws Exception {
-        Assert.fail();
+        //TODO: Extract the 2 lines below into a method called "getJsonData(jsonResourceFile)"
+        ClassPathResource postJsonResourceFile = new ClassPathResource("testData/postAlert/postAlertMustReturnBadRequestForNullDelay.json");
+        byte[] alertJsonWithNullDelay = Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+
+        this.mockMvc.perform(post(ALERTS_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(alertJsonWithNullDelay))
+                .andExpect(status().isBadRequest())
+                .andReturn();
     }
 
 
     @Test
     public void postAlertMustReturnBadRequestForNullDescription() throws Exception {
-        Assert.fail();
+        //TODO: Extract the 2 lines below into a method called "getJsonData(jsonResourceFile)"
+        ClassPathResource postJsonResourceFile = new ClassPathResource("testData/postAlert/postAlertMustReturnBadRequestForNullDescription.json");
+        byte[] alertJsonWithNullDescription = Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+
+        this.mockMvc.perform(post(ALERTS_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(alertJsonWithNullDescription))
+                .andExpect(status().isBadRequest())
+                .andReturn();
     }
 
 
     @Test
     public void postAlertMustReturnBadRequestIfAlertAlreadyExists() throws Exception {
-        Assert.fail();
+        //TODO: Extract the 2 lines below into a method called "getJsonData(jsonResourceFile)"
+        ClassPathResource postJsonResourceFile = new ClassPathResource("testData/postAlert/postAlertMustReturnCreatedForValidAlert.json");
+        byte[] validAlertJson = Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+
+        //Post a valid alert should succeed
+        this.mockMvc.perform(post(ALERTS_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(validAlertJson))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        //Re-posting same alert again should fail
+        this.mockMvc.perform(post(ALERTS_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(validAlertJson))
+                .andExpect(status().isBadRequest())
+                .andReturn();
     }
 
 
     @Test
     public void postAlertMustReturnCreatedForAValidAlert() throws Exception {
-        Assert.fail();
+        //TODO: Extract the 2 lines below into a method called "getJsonData(jsonResourceFile)"
+        ClassPathResource postJsonResourceFile = new ClassPathResource("testData/postAlert/postAlertMustReturnCreatedForValidAlert.json");
+        byte[] validAlertJson = Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+
+        //Post a valid alert should succeed
+        this.mockMvc.perform(post(ALERTS_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(validAlertJson))
+                .andExpect(status().isCreated())
+                .andReturn();
     }
 }
