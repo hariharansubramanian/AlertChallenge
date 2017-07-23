54222561d8ecb12793dbbe10b49c4a5737ab722d
Hariharan Subramanian <hariharan1990.s@gmail.com>
2017-07-23 20:19:34 +0530

    Added a new Module for Integration testing, Now able to run test cases with 'gradlew integrationTest' * Configured WebIntegrationTestBase which serves as the core of all integration test cases * Downgraded gradle wrapper jar to 2.14.1 due to integration issues

--- src/test/java/life/dev/hari/alertChallenge/AlertChallengeApplicationTests.java
+++ src/test/java/life/dev/hari/alertChallenge/AlertChallengeApplicationTests.java
@@ -1,16 +1,4 @@
 package life.dev.hari.alertChallenge;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class AlertChallengeApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
 }
