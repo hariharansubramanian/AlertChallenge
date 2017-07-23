8a7dfe0129a401a5e7a12d2a9a1a00b50902b4f8
Hariharan Subramanian <hariharan1990.s@gmail.com>
2017-07-23 17:22:50 +0530

    Created a Gradle project using the Spring framework -- Contains gradlew, gradlew.bat, gradle-wrapper.properties in order to auto-generate the gradlew-wrapper.jar and execute gradle commands in Windows, Linux or Mac OS.
    -- Added the following external project dependencies in build.gradle
       * spring-boot-starter
       * spring-data-rest
       * spring-boot-starter-actuator
       * jackson JSON serialization/deserialization
       * postgresql drivers bridge
       * spring-boot-starter-test for integration testing
       * junit unit testing and basic assert requirements
    
    -- Added resources/application.yml for environment based project configuration settings
       * Setup environment profiles: 'common', 'local', 'dev', 'test', 'pre-prod' and 'prod', with default profile set to 'common,local'
       * Setup server context, port, logging, datasources and enabled CORS.
    
    -- Created AlertChallengeApplication.java as the main container running the SpringBootApplication
    -- Created Test directory and configured it to run with SpringRunner

---
+++ src/main/java/life/dev/hari/alertChallenge/AlertChallengeApplication.java
@@ -0,0 +1,12 @@
+package life.dev.hari.alertChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertChallengeApplication.class, args);
+	}
+}
