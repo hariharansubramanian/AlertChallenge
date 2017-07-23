5d5dbf7d041abb994e242aa93c874e029b98b36a
Hariharan Subramanian <hariharan1990.s@gmail.com>
2017-07-24 04:18:10 +0530

    * Created generic JsonDeserializer which can convert any JsonString into a custom object or List of custom objects * Wrote integration test cases covering Scenarios of GETing list of all alerts.

--- src/integrationTest/java/life.dev.hari.alertChallenge/controller/AlertControllerIntegrationTest.java
+++ src/integrationTest/java/life.dev.hari.alertChallenge/controller/AlertControllerIntegrationTest.java
@@ -1,9 +1,10 @@
 package life.dev.hari.alertChallenge.controller;
 
 import life.dev.hari.alertChallenge.WebIntegrationTestBase;
+import life.dev.hari.alertChallenge.model.Alert;
 import life.dev.hari.alertChallenge.repository.AlertRepository;
+import life.dev.hari.alertChallenge.utilities.JsonDeserializer;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -11,8 +12,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
+import org.springframework.util.Assert;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -44,9 +48,7 @@ public class AlertControllerIntegrationTest extends WebIntegrationTestBase {
      */
     @Test
     public void postAlertMustReturnBadRequestForNullReferenceId() throws Exception {
-        //TODO: Extract the 2 lines below into a method called "getJsonData(jsonResourceFile)"
-        ClassPathResource postJsonResourceFile = new ClassPathResource("testData/postAlert/postAlertMustReturnBadRequestForNullReferenceId.json");
-        byte[] alertJsonWithNullReferenceId = Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+        byte[] alertJsonWithNullReferenceId = getJsonData("testData/postAlert/postAlertMustReturnBadRequestForNullReferenceId.json");
 
         this.mockMvc.perform(post(ALERTS_URL_PATH)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -58,9 +60,7 @@ public class AlertControllerIntegrationTest extends WebIntegrationTestBase {
 
     @Test
     public void postAlertMustReturnBadRequestForNullDelay() throws Exception {
-        //TODO: Extract the 2 lines below into a method called "getJsonData(jsonResourceFile)"
-        ClassPathResource postJsonResourceFile = new ClassPathResource("testData/postAlert/postAlertMustReturnBadRequestForNullDelay.json");
-        byte[] alertJsonWithNullDelay = Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+        byte[] alertJsonWithNullDelay = getJsonData("testData/postAlert/postAlertMustReturnBadRequestForNullDelay.json");
 
         this.mockMvc.perform(post(ALERTS_URL_PATH)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -72,9 +72,7 @@ public class AlertControllerIntegrationTest extends WebIntegrationTestBase {
 
     @Test
     public void postAlertMustReturnBadRequestForNullDescription() throws Exception {
-        //TODO: Extract the 2 lines below into a method called "getJsonData(jsonResourceFile)"
-        ClassPathResource postJsonResourceFile = new ClassPathResource("testData/postAlert/postAlertMustReturnBadRequestForNullDescription.json");
-        byte[] alertJsonWithNullDescription = Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+        byte[] alertJsonWithNullDescription = getJsonData("testData/postAlert/postAlertMustReturnBadRequestForNullDescription.json");
 
         this.mockMvc.perform(post(ALERTS_URL_PATH)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -86,9 +84,7 @@ public class AlertControllerIntegrationTest extends WebIntegrationTestBase {
 
     @Test
     public void postAlertMustReturnBadRequestIfAlertAlreadyExists() throws Exception {
-        //TODO: Extract the 2 lines below into a method called "getJsonData(jsonResourceFile)"
-        ClassPathResource postJsonResourceFile = new ClassPathResource("testData/postAlert/postAlertMustReturnCreatedForValidAlert.json");
-        byte[] validAlertJson = Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+        byte[] validAlertJson = getJsonData("testData/postAlert/postAlertMustReturnCreatedForValidAlert.json");
 
         //Post a valid alert should succeed
         this.mockMvc.perform(post(ALERTS_URL_PATH)
@@ -108,9 +104,7 @@ public class AlertControllerIntegrationTest extends WebIntegrationTestBase {
 
     @Test
     public void postAlertMustReturnCreatedForAValidAlert() throws Exception {
-        //TODO: Extract the 2 lines below into a method called "getJsonData(jsonResourceFile)"
-        ClassPathResource postJsonResourceFile = new ClassPathResource("testData/postAlert/postAlertMustReturnCreatedForValidAlert.json");
-        byte[] validAlertJson = Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+        byte[] validAlertJson = getJsonData("testData/postAlert/postAlertMustReturnCreatedForValidAlert.json");
 
         //Post a valid alert should succeed
         this.mockMvc.perform(post(ALERTS_URL_PATH)
@@ -130,22 +124,48 @@ public class AlertControllerIntegrationTest extends WebIntegrationTestBase {
                 .perform(get(ALERTS_URL_PATH))
                 .andExpect(status().isOk())
                 .andReturn().getResponse();
-        logger.debug("Returned list of alerts: "+ mockHttpServletResponse.getContentAsString());
+        logger.debug("Returned list of alerts: " + mockHttpServletResponse.getContentAsString());
+
+        List<Alert> alerts = deserializeAlertsJson(mockHttpServletResponse.getContentAsString());
+        Assert.isTrue(alerts.size() == 0);
     }
 
     @Test
     public void getAlertsMustReturnOnlyAlertsWhichHaveCrossedTheDelayThreshold() throws Exception {
         //Get alerts should succeed and return
-            //* List of alerts which have crossed the delay threshold
+        //* List of alerts which have crossed the delay threshold
 
         //TODO: Register various alerts with different delays
         mockHttpServletResponse = this.mockMvc
                 .perform(get(ALERTS_URL_PATH))
                 .andExpect(status().isOk())
                 .andReturn().getResponse();
-        logger.debug("Returned list of alerts: "+ mockHttpServletResponse.getContentAsString());
+        logger.debug("Returned list of alerts: " + mockHttpServletResponse.getContentAsString());
 
         //TODO: Assert only alerts crossing the delay threshold are returned
     }
 
+
+    /**
+     * Utility method to convert Json file contents into bytes
+     * @param filePath - Json file path to be used in test case
+     * @return - Byte stream of json raw body
+     * @throws IOException
+     */
+    private byte[] getJsonData(String filePath) throws IOException {
+        ClassPathResource postJsonResourceFile = new ClassPathResource(filePath);
+        return Files.readAllBytes(postJsonResourceFile.getFile().toPath());
+    }
+
+    /**
+     * Utility method to Convert response stream into an array of custom objects
+     * In this case converting response body to an array of Alert Objects
+     * @param responseJson - Response body contents as string
+     * @return - Array of Alert Objects
+     * @throws IOException
+     */
+    private List<Alert> deserializeAlertsJson(String responseJson) throws IOException {
+        JsonDeserializer<Alert> deserializedAlerts = new JsonDeserializer<>(jsonFactory, objectMapper, Alert.class);
+        return deserializedAlerts.getObjectList(responseJson);
+    }
 }
---
+++ src/main/java/life/dev/hari/alertChallenge/utilities/JsonDeserializer.java
@@ -0,0 +1,60 @@
+package life.dev.hari.alertChallenge.utilities;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+/**
+ * Created by plank-hari.s on 7/24/2017.
+ */
+public class JsonDeserializer<T> {
+    private final JsonFactory jsonFactory;
+    private final ObjectMapper objectMapper;
+    private Class<T> typeT;
+
+    public JsonDeserializer(JsonFactory factory, ObjectMapper mapper, Class<T> clazz) {
+        jsonFactory = factory;
+        objectMapper = mapper;
+        typeT = clazz;
+    }
+
+
+    public List<T> getObjectList(String jsonData) throws IOException {
+
+        List<T> objects = new ArrayList<T>();
+        ArrayList<T> allEventsSentViaRest = new ArrayList<T>();
+        JsonParser jsonParser = jsonFactory.createParser(jsonData);
+
+        jsonParser.nextToken();
+
+        while (jsonParser.nextToken() == JsonToken.START_OBJECT) {
+            T t = objectMapper.readValue(jsonParser, typeT);
+            assertThat(String.format("%s object should not be null", t.getClass().getName()),
+                    t, is(notNullValue()));
+            objects.add(t);
+        }
+        return objects;
+    }
+
+
+    public T getObject(String jsonData) throws IOException {
+
+        T t;
+        JsonParser jsonParser = jsonFactory.createParser(jsonData);
+
+        if (jsonParser.nextToken() == JsonToken.START_OBJECT) {
+            t = objectMapper.readValue(jsonParser, typeT);
+            assertThat(String.format("%s object should not be null", t.getClass().getName()),
+                    t, is(notNullValue()));
+            return t;
+        }
+        return null;
+    }
+}
