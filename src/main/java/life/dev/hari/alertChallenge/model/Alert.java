98d44954d5122e4edf24246bb193f0b77a63305e
Hariharan Subramanian <hariharan1990.s@gmail.com>
2017-07-23 20:33:59 +0530

    Constructed the Alert model with the following attributes: * dateCreated: Date * referenceId: String * delay: integer * description: String

--- src/main/java/life/dev/hari/alertChallenge/model/Alert.java
+++ src/main/java/life/dev/hari/alertChallenge/model/Alert.java
@@ -1,7 +1,12 @@
 package life.dev.hari.alertChallenge.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 /**
  * Created by plank-hari.s on 7/23/2017.
@@ -10,4 +15,22 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "alert")
 public class Alert extends AbstractEntity {
+
+    @NotNull
+    @Column(name = "date_created")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private Date dateCreated;
+
+    @NotNull
+    @Column(name = "reference_id")
+    public String referenceId;
+
+    @NotNull
+    @Column(name = "delay")
+    public Integer delay = 0;
+
+    @NotNull
+    @Column(name = "description")
+    private String description;
+
 }
